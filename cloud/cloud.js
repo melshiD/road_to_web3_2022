5b357b4a54dea20d5697170127da762b3c148b80
David Melsheimer <dave.melshman@gmail.com>
2022-02-06 18:38:51 -0500

    test commit

--- cloud/cloud.js
+++ cloud/cloud.js
@@ -7,3 +7,5 @@ Moralis.Cloud.define('getUserTransactionList', async (request) => {
 //Command script for listening to cloud folder
 // moralis-admin-cli watch-cloud-folder --moralisApiKey ohTeQrLHl8DL0xN --moralisApiSecret 0rCf5JScGiN0Ssq --moralisCloudfolder C:/"Program Files"/User/davem/Coding/Hackathon/road_to_web3_2022/cloud --moralisSubdomain w3lanyjsypwl.usemoralis.com --autoSave 1
 // | Listening folder: C:/Users1/davem/Coding/Hackathon/road_to_web3_2022/cloud
+
+
